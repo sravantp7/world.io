586095b2671e96c84d0f02e13ceb621ddd3a4dcc
Sravan T P <sravantp99@gmail.com>
2024-08-21 14:18:53 +0530

    update files

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,15 @@
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNavigation from "./AppNavigation";
+import { Outlet } from "react-router-dom";
 
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNavigation />
-      <p>Countries List</p>
+      {/* Render nested route */}
+      <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by World.io
