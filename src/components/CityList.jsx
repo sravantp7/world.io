39613e08a4b51ecc3340ba47f836b88fe9091290
Sravan T P <sravantp99@gmail.com>
2024-08-22 10:08:21 +0530

    update citylist component

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,20 +1,22 @@
 import styles from "./CityList.module.css";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
 
+// Convert date to given format
+function getDate(date) {
+  const day = date.getDate();
+  // Get full month name from date
+  const month = date.toLocaleString("default", { month: "long" });
+  const year = date.getFullYear();
+
+  return `${month} ${day}, ${year}`;
+}
+
 export default function CityList() {
+  // Getting data from the loader function in the route
   const { cities } = useLoaderData();
+
   //   const [cities, setCities] = useState([]);
-
-  // Convert date to given format
-  function getDate(date) {
-    const day = date.getDate();
-    // Get full month name from date
-    const month = date.toLocaleString("default", { month: "long" });
-    const year = date.getFullYear();
-
-    return `${month} ${day}, ${year}`;
-  }
 
   // Fetching cities list from json-server
   /*
@@ -30,18 +32,22 @@ export default function CityList() {
   return (
     <ul className={styles.cityList}>
       {cities.length > 0 &&
-        cities.map((city) => (
-          <li key={city.id} className={styles.city}>
-            <div>
-              <span>{city.emoji}</span>
-              <span>{city.cityName}</span>
-            </div>
-            <div>
-              <span>({getDate(new Date(city.date))})</span>
-              <button>x</button>
-            </div>
-          </li>
-        ))}
+        cities.map((city) => <CityItem key={city.id} city={city} />)}
     </ul>
+  );
+}
+
+function CityItem({ city }) {
+  return (
+    <li className={styles.city}>
+      <div>
+        <span>{city.emoji}</span>
+        <span>{city.cityName}</span>
+      </div>
+      <div>
+        <span>({getDate(new Date(city.date))})</span>
+        <button>x</button>
+      </div>
+    </li>
   );
 }
