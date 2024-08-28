8002c6305827608167fb5500b0890a61590f36b9
Sravan T P <sravantp99@gmail.com>
2024-08-28 13:48:12 +0530

    add city markers in map & implement map view updation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,16 @@
 import styles from "./Map.module.css";
 import { useSearchParams, useNavigate } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import { useEffect, useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 export default function Map() {
   // Here searchParams is of type URLSearchParams and contains function to access its values
   const [searchParams, setSearchParams] = useSearchParams();
 
+  const { cities } = useCities();
+
+  // State which is used to move map view when a city from the citylist is clicked
   const [mapPos, setMapPos] = useState([40, 0]);
 
   function updateMyLocation() {
@@ -14,8 +18,7 @@ export default function Map() {
       const latitude = pos.coords.latitude;
       const longitude = pos.coords.longitude;
 
-      // Updating URL params programatically
-      setSearchParams({ lat: latitude, lng: longitude });
+      setMapPos([latitude, longitude]);
     });
   }
 
@@ -25,21 +28,38 @@ export default function Map() {
   useEffect(() => {
     // if searchParams exists then size will be greater than 0
     if (searchParams.size > 0) {
-      setMapPos([searchParams.get("lat"), searchParams.get("lng")]);
+      // getting selected city pos from the url and update the state
+      setMapPos([+searchParams.get("lat"), +searchParams.get("lng")]);
     }
   }, [searchParams]);
 
   return (
-    <MapContainer center={mapPos} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={mapPos} zoom={8} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
       />
-      {/* <Marker position={mapPos}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker> */}
+
+      {/* Rendering marker for each cities */}
+      {cities.map(({ id, position, cityName, emoji }) => (
+        <Marker position={[position.lat, position.lng]} key={id}>
+          <Popup>
+            <span>{emoji}</span>
+            <span>{cityName}</span>
+          </Popup>
+        </Marker>
+      ))}
+
+      {/* Updating map view when the postion changes */}
+      <ChangeMapView position={mapPos} />
     </MapContainer>
   );
+}
+
+// Our own custom component for view updation
+// Component resposible to changing map view when postion changes [react leaflet way of changing view as per doc]
+function ChangeMapView({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
 }
