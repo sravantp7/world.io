b7b2ab72e1b77b77d29a969f2875bc0b4759579c
Sravan T P <sravantp99@gmail.com>
2024-08-28 12:39:57 +0530

    add map using leaflet

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,13 @@
 import styles from "./Map.module.css";
 import { useSearchParams, useNavigate } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useEffect, useState } from "react";
 
 export default function Map() {
   // Here searchParams is of type URLSearchParams and contains function to access its values
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const [mapPos, setMapPos] = useState([40, 0]);
 
   function updateMyLocation() {
     navigator.geolocation.getCurrentPosition((pos) => {
@@ -20,11 +21,25 @@ export default function Map() {
 
   const navigate = useNavigate();
 
+  // Update map position when we get lat and lng via seach params
+  useEffect(() => {
+    // if searchParams exists then size will be greater than 0
+    if (searchParams.size > 0) {
+      setMapPos([searchParams.get("lat"), searchParams.get("lng")]);
+    }
+  }, [searchParams]);
+
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <p>Lat: {lat}</p>
-      <p>Lng: {lng}</p>
-      <button onClick={updateMyLocation}>updateLocation</button>
-    </div>
+    <MapContainer center={mapPos} zoom={13} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+      />
+      {/* <Marker position={mapPos}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker> */}
+    </MapContainer>
   );
 }
