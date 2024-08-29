111bdbbc985b015adba0b335f1d96fc61e7796e4
Sravan T P <sravantp99@gmail.com>
2024-08-29 12:35:36 +0530

    add city selection & update form with the selected city data

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,16 @@
 import styles from "./Form.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+
+// Function for converting country code to emoji
+function countryCodeToFlagEmoji(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt(0));
+
+  return String.fromCodePoint(...codePoints);
+}
 
 export default function Form() {
   // reads data from the url query
@@ -10,10 +20,58 @@ export default function Form() {
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [emoji, setEmoji] = useState("");
+  const [error, setError] = useState(false);
 
   const navigate = useNavigate();
 
-  console.log(searchParams);
+  // Gets the user clicked location's lat and lng value.
+  const lat = +searchParams.get("lat");
+  const lng = +searchParams.get("lng");
+
+  useEffect(() => {
+    // Fetch cities information using the lat and lng from another api
+    // api -> https://api-bdc.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en
+    async function fetchCity() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api-bdc.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
+        );
+        const data = await res.json();
+
+        // If our selection is not a country then it will throw error
+        // and displays error message instead of form in the ui
+        if (!data.countryCode) {
+          throw new Error("It is not a valid city, Please try again!");
+        }
+
+        // State updates [Batched]
+        setError(false);
+        setCityName(data.city || data.locality);
+        setCountry(data.countryName);
+        setEmoji(countryCodeToFlagEmoji(data.countryCode));
+      } catch (error) {
+        setError(true);
+        console.error(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCity();
+  }, [lat, lng]);
+
+  if (error) {
+    return (
+      <p className={styles.message}>It's not a valid city, Please try again!</p>
+    );
+  }
+
+  if (isLoading) {
+    return <p className={styles.message}>Loading city information ...</p>;
+  }
 
   return (
     <form className={styles.form}>
@@ -22,7 +80,8 @@ export default function Form() {
         <input
           id="cityName"
           onChange={(e) => setCityName(e.target.value)}
-          value={cityName}
+          disabled
+          value={`${emoji} ${cityName}`}
         />
         {/* <span className={styles.flag}>{emoji}</span> */}
       </div>
