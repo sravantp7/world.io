91114d45339d6b65b8a1b4919db48db04c1c2cec
Sravan T P <sravantp99@gmail.com>
2024-08-30 15:45:55 +0530

    update CitiesContext state management using useReducer hook

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,27 +1,68 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useReducer,
+} from "react";
 
 // Creating a Context
 const CitiesContext = createContext();
 
 const BASE_URL = "http://localhost:8000/cities";
 
+// Initial reducer state
+const initialState = {
+  cities: [],
+  currentCity: {},
+  isLoading: false,
+};
+
+// Reducer function that manages state updates
+function reducer(state, action) {
+  switch (action.type) {
+    case "updateCities":
+      return {
+        ...state,
+        cities: action.payload,
+      };
+    case "loading":
+      return {
+        ...state,
+        isLoading: action.payload,
+      };
+    case "updateCurrentCity":
+      return {
+        ...state,
+        currentCity: action.payload,
+      };
+  }
+}
+
 // Custom context provider
 export function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
+  // const [cities, setCities] = useState([]);
 
   // State stores the current clicked city
-  const [currentCity, setCurrentCity] = useState({});
+  /* const [currentCity, setCurrentCity] = useState({}); */
 
   // State for tracking new city creation
-  const [isLoading, setIsLoading] = useState(false);
+  /* const [isLoading, setIsLoading] = useState(false); */
 
+  // state management
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { cities, isLoading, currentCity } = state;
+
+  // Runs after initial rendering
   useEffect(() => {
     // Fetch cities information
     async function fetchCities() {
       try {
         const res = await fetch(BASE_URL);
         const data = await res.json();
-        setCities(data);
+        // setCities(data);
+        dispatch({ type: "updateCities", payload: data });
       } catch (error) {
         console.error(error.message);
       }
@@ -33,7 +74,8 @@ export function CitiesProvider({ children }) {
   // Function that creates new city object in the cities.json file
   async function createNewCity(newCity) {
     try {
-      setIsLoading(true);
+      // setIsLoading(true);
+      dispatch({ type: "loading", payload: true });
 
       // Creating new city
       const res = await fetch(BASE_URL, {
@@ -50,18 +92,22 @@ export function CitiesProvider({ children }) {
       }
 
       // Updating city state with new city.
-      setCities((cities) => [...cities, new_city]);
+      /* setCities((cities) => [...cities, new_city]);  */
+      dispatch({ type: "updateCities", payload: [...cities, new_city] });
     } catch (error) {
       console.log(error.message);
     } finally {
-      setIsLoading(false);
+      // setIsLoading(false);
+      dispatch({ type: "loading", payload: false });
     }
   }
 
   // Delete city
   async function deleteCity(cityId) {
     try {
-      setIsLoading(true);
+      /* setIsLoading(true); */
+      dispatch({ type: "loading", payload: true });
+
       // request to json-server for deleting specific city from the cities.json file
       const res = await fetch(`${BASE_URL}/${cityId}`, {
         method: "DELETE",
@@ -69,12 +115,19 @@ export function CitiesProvider({ children }) {
       await res.json();
 
       // Filtering cities state by removing the city with id cityId
-      setCities((cities) => cities.filter((city) => city.id != cityId));
+      /* setCities((cities) => cities.filter((city) => city.id != cityId)); */
+      const updatedCities = cities.filter((city) => city.id != cityId); // calculate new list of cities
+      dispatch({ type: "updateCities", payload: updatedCities });
     } catch (error) {
       console.log(error.message);
     } finally {
-      setIsLoading(false);
+      /* setIsLoading(false); */
+      dispatch({ type: "loading", payload: false });
     }
+  }
+
+  function setCurrentCity(city) {
+    dispatch({ type: "updateCurrentCity", payload: city });
   }
 
   return (
